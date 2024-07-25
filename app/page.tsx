2570f84011da9316b34302b5f4c178a1de9e07c5
KEENTO <handball0321@gmail.com>
2024-07-25 20:59:44 +0900

    Add: HomePage component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { getClient } from "./lib/client";
 import styles from "./page.module.css";
 import { TestQuery } from "./graphql/generated";
 import { QUERY_GET_TEST } from "./graphql/client";
+import { HomePage } from "./HomePage";
 
 export default async function Home() {
   const {
@@ -11,15 +12,11 @@ export default async function Home() {
   } = await getClient().query<TestQuery>({
     query: QUERY_GET_TEST,
   });
-  console.log("films: ", data_test.allFilms?.films);
+
   if (loading) return <div>Loading now...</div>;
   if (error) {
     return <div>{error.message}</div>;
   }
 
-  return (
-    <main className={styles.main}>
-      <div className="">aaa</div>
-    </main>
-  );
+  return <HomePage data={data_test} />;
 }
