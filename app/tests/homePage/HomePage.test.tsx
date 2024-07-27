c24753a56f91b17c8d992aa0bf97f9dfc2a48f45
KEENTO <handball0321@gmail.com>
2024-07-27 17:49:25 +0900

    Fix: setup jest

--- app/tests/homePage/HomePage.test.tsx
+++ app/tests/homePage/HomePage.test.tsx
@@ -1,10 +1,67 @@
-import { FilmsQuery } from "@/app/graphql/generated";
 import { HomePage } from "@/app/HomePage";
+import { render, screen } from "@testing-library/react";
+import { MockedResponse } from "@apollo/client/testing";
+import { QUERY_FILMS } from "@/app/graphql/client";
+import { Film, FilmsQuery } from "@/app/graphql/generated";
+import "@testing-library/jest-dom";
 
-// describe("homePage", () => {
-//   const mockData: FilmsQuery = { allFilms: [] };
+const mockData: MockedResponse<FilmsQuery>[] = [
+  {
+    request: {
+      query: QUERY_FILMS,
+    },
+    result: {
+      data: {
+        allFilms: {
+          films: [
+            {
+              id: "1",
+              title: "A New Hope",
+              releaseDate: "1977-05-25",
+              created: "2021-01-01",
+              director: "George Lucas",
+              characterConnection: {
+                __typename: "FilmCharactersConnection",
+                totalCount: 10,
+                characters: [
+                  {
+                    __typename: "Person",
+                    birthYear: "19BBY",
+                    created: "2021-01-01",
+                    edited: "2021-01-01",
+                  },
+                ],
+              },
+            },
+          ],
+        },
+      },
+    },
+  },
+];
 
-//   it("should render data when the page is loaded", () => {
-//     render(<HomePage data={mockData} />);
-//   });
-// });
+const mockFilms: Film[] = [
+  {
+    id: "1",
+    title: "A New Hope",
+    releaseDate: "1977-05-25",
+    created: "2021-01-01",
+    director: "George Lucas",
+  },
+];
+
+describe("homePage", () => {
+  const renderComp = () => {
+    const data = mockFilms;
+
+    render(<HomePage data={data} />);
+  };
+
+  it("should render data when the page is loaded", () => {
+    renderComp();
+
+    const title = screen.getByText(/page/i);
+
+    expect(title).toBeInTheDocument();
+  });
+});
