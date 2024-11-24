91afd05f952b86049a0a7e57ec766f3a363abf4e
KEENTO <handball0321@gmail.com>
2024-11-24 14:19:06 +0900

    Add: LoadingProfile component

--- app/_containers/dashboard/container.tsx
+++ app/_containers/dashboard/container.tsx
@@ -3,11 +3,12 @@ import { DashBoardPresentation } from "./presentation";
 import { PostListContainer } from "@/app/_components/lists/postList";
 import { LoadingSpinner } from "@/app/_components/loadingSpinner/LoadingSpinner";
 import { UserProfileContainer } from "@/app/_components/userProfile";
+import { LoadingProfile } from "@/app/_components/loadingProfile/LoadingProfile";
 
 export const DashBoardContainer = () => {
   return (
     <DashBoardPresentation>
-      <Suspense>
+      <Suspense fallback={<LoadingProfile />}>
         <UserProfileContainer />
       </Suspense>
       <Suspense fallback={<LoadingSpinner />}>
